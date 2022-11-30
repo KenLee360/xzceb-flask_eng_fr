db105c3cd264982ddb24297aa9ed9f61ee3f967c
KenLee360 <112226618+KenLee360@users.noreply.github.com>
2022-11-29 22:45:27 -0500

    Update translator.py

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -9,7 +9,7 @@ from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 #apikey = os.environ['j4PwIL9v2Wv-7XCvUISYQzvAQ91J7N3imSsxadNYVfq_']
 #url = os.environ['https://api.eu-gb.language-translator.watson.cloud.ibm.com/instances/9074e938-5391-4ab9-b292-1c1b6825b51e']
 
-authenticator = IAMAuthenticator('j4PwIL9v2Wv-7XCvUISYQzvAQ91J7N3imSsxadNYVfq_')
+authenticator = IAMAuthenticator('#Removed API Key#')
 language_translator = LanguageTranslatorV3(
     version='2018-05-01',
     authenticator=authenticator
