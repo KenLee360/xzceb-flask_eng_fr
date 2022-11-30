873399516bdec76e547fdaf3a5b1fbe4bc8ee394
KenLee360 <kennylee360@gmail.com>
2022-11-30 16:11:01 -0500

    Added files

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,45 +1,43 @@
 import json
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-#import os
-#from dotenv import load_dotenv
 
-#load_dotenv()
-
-#apikey = os.environ['j4PwIL9v2Wv-7XCvUISYQzvAQ91J7N3imSsxadNYVfq_']
-#url = os.environ['https://api.eu-gb.language-translator.watson.cloud.ibm.com/instances/9074e938-5391-4ab9-b292-1c1b6825b51e']
-
-authenticator = IAMAuthenticator('j4PwIL9v2Wv-7XCvUISYQzvAQ91J7N3imSsxadNYVfq_')
+#Passes in API Key and URL to use language tranlsator
+authenticator = IAMAuthenticator('#Removed API key')
 language_translator = LanguageTranslatorV3(
     version='2018-05-01',
     authenticator=authenticator
 )
 
-language_translator.set_service_url('https://api.eu-gb.language-translator.watson.cloud.ibm.com/instances/9074e938-5391-4ab9-b292-1c1b6825b51e')
+language_translator.set_service_url(
+    'https://api.eu-gb.language-translator.watson.cloud.ibm.com/instances/9074e938-5391-4ab9-b292-1c1b6825b51e'
+)
 
-def englishToFrench():
-    englishText = str(input('Enter English Text: '))
-    frenchText = language_translator.translate(
-        text=englishText,model_id='en-fr').get_result()
-    print(englishText)
-    print(frenchText)
+def english_to_french(french_text):
+    #Converts English Text to French Text outputs as JSON
+    french_text = language_translator.translate(
+        text=french_text,model_id='en-fr').get_result()
+    return french_text
 
-def frenchToEnglish():
-    frenchText = str(input('Enter French Text: '))
-    englishText = language_translator.translate(
-        text=frenchText,model_id='fr-en').get_result()
-    print(frenchText)
-    print(englishText)
+def french_to_english(english_text):
+    ##Converts French Text to English Text outputs as JSON
+    english_text = language_translator.translate(
+        text=english_text,model_id='fr-en').get_result()
+    return english_text
 
 
 while True:
-    choice = str(input('English or French? '))
-    if choice == 'English':
-        englishToFrench()
+    #Takes in Text and from Language
+    WORD = str(input('Please Enter text: '))
+    CHOICE = str(input('To English or French? '))
+    if CHOICE == 'French':
+        #Runs function called above, converts to French
+        Translated = english_to_french(WORD)
+        print(json.dumps(Translated))
         break
-    if choice == 'French':
-        frenchToEnglish()
+    if CHOICE == 'English':
+        #Runs function called above, converts to English
+        Translated = french_to_english(WORD)
+        print(json.dumps(Translated))
         break
-    else:
-        print('Not a valid Answer.')
-
+#Program Complete
