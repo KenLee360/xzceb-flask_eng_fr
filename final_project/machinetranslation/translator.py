253dc025628674a57f5e495031c57244c5edc0ef
KenLee360 <kennylee360@gmail.com>
2022-11-30 20:41:07 -0500

    Updated server to add endpoints

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -2,7 +2,7 @@ import json
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
-authenticator = IAMAuthenticator('Removed API Key')
+authenticator = IAMAuthenticator('API Key')
 language_translator = LanguageTranslatorV3(
     version='2018-05-01',
     authenticator=authenticator
